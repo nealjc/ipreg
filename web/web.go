1b54340c3ed61de32f41c2ced238b462e27352ce
Neal <ncharbonneau@gmail.com>
2014-01-08 20:58:52 -0500

    Adding very basic REST-ish API for getting node status

--- web/web.go
+++ web/web.go
@@ -1,6 +1,20 @@
 /*
 Package web is responsible for serving a REST API for retreiving IP status and reserving
 IPs.
+
+The available URLs are:
+
+GETs
+/subnets
+/addresses/<subnet name>
+/status/<address>
+
+PUTs
+/status/<address>
+
+DELETE
+/status/<address>
+
 */
 package web
 
@@ -11,25 +25,12 @@ import(
 	"github.com/nealjc/ipreg/scanner"
 	"encoding/json"
 	"log"
+	"strings"
 )
-
-type StatusPage struct {
-	StatusMap map[string]*scanner.NodeStatus
-}
-
-var serverControl chan bool
-
-func (s *StatusPage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(s.StatusMap)
-	if err != nil {
-		fmt.Fprintf(w, "Error")
-	}
-	fmt.Fprintf(w, "%s", b)
-}
 
 func StartServer(status []*scanner.Subnet) {
 	serverControl = make(chan bool)
-	statusPage := StatusPage{status[0].Nodes}
+	statusPage := StatusPage{status}
 	server := http.Server{
 		Addr: ":8080",
 		Handler: nil,
@@ -56,3 +57,107 @@ func StopServer() {
 	<- serverControl
 }
 
+type StatusPage struct {
+	Subnets []*scanner.Subnet
+}
+
+var serverControl chan bool
+
+func (s *StatusPage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.Split(r.URL.Path, "/")
+	log.Printf("Request to %q", path[1])
+	switch (path[1]) {
+	case "subnets":
+		s.handleSubnets(w, r)
+	case "addresses":
+		s.handleAddresses(w, r)
+	case "status":
+		s.handleStatus(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func (s *StatusPage) handleSubnets(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO: index out of bounds error
+	subnets := make([][]string, len(s.Subnets), len(s.Subnets))
+	for i, subnet := range(s.Subnets) {
+		subnets[i] = make([]string, 2, 2)
+		subnets[i][0] = subnet.Name
+		subnets[i][1] = subnet.Network
+	}
+	b, err := json.Marshal(subnets)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", b)
+}
+
+func (s *StatusPage) handleAddresses(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO: index out of bounds error
+	subnetIndex := s.lookupSubnet(strings.Split(r.URL.Path, "/")[2])
+	if subnetIndex == -1 {
+		w.WriteHeader(http.StatusNotFound)
+		return 
+	}
+	
+	subnet := make([]string, len(s.Subnets[subnetIndex].OrderedAddresses),
+		len(s.Subnets[subnetIndex].OrderedAddresses))
+	for i, address := range(s.Subnets[subnetIndex].OrderedAddresses) {
+		subnet[i] = address
+	}
+	b, err := json.Marshal(subnet)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", b)
+}
+
+func (s *StatusPage) lookupSubnet(name string) int {
+	for i, subnet := range(s.Subnets) {
+		if subnet.Name == name {
+			return i
+		}
+	}
+	return -1
+}
+
+func (s *StatusPage) handleStatus(w http.ResponseWriter, r *http.Request) {
+
+	address := strings.Split(r.URL.Path, "/")[2]
+	for _, subnet := range(s.Subnets) {
+		if status, ok := subnet.Nodes[address]; ok {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "%s", formatStatus(status))
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func formatStatus(status *scanner.NodeStatus) string {
+	switch status.Status {
+	case scanner.DOWN:
+		return "Down"
+	case scanner.UP:
+		return "Up"
+	case scanner.UNKNOWN:
+		return "Unknown"
+	default:
+		return "Invalid state"
+	}
+}
