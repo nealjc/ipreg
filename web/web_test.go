508809a07c435ee57fcbb569df15afe13695f1b8
Neal <ncharbonneau@gmail.com>
2014-01-18 21:32:47 -0500

    Outline of tests for web.go

---
+++ web/web_test.go
@@ -0,0 +1,59 @@
+package web
+
+import (
+	"testing"
+)
+
+func TestFailedStart(t *testing.T) {
+}
+
+func TestGetGoodSubnet(t *testing.T) {
+}
+
+func TestGetBadSubnet(t *testing.T) {
+}
+
+func TestGetGoodAddresses(t *testing.T) {
+}
+
+func TestGetBadAddresses(t *testing.T) {
+}
+
+func TestGetGoodStatus(t *testing.T) {
+}
+
+func TestGetBadStatus(t *testing.T) {
+}
+
+func TestPutGoodStatus(t *testing.T) {
+}
+
+func TestPutBadStatus(t *testing.T) {
+}
+
+func TestDeleteGoodStatus(t *testing.T) {
+}
+
+func TestDeleteBadStatus(t *testing.T) {
+}
+
+func TestInvalidGetRequests(t *testing.T) {
+}
+
+func TestInvalidPutRequests(t *testing.T) {
+}
+
+func TestInvalidDeleteRequests(t *testing.T) {
+}
+
+func TestBadUrls(t *testing.T) {
+	// only index.html should be good...
+}
+
+func TestConcurrentAccess(t *testing.T) {
+}
+
+
+func initServer(t *testing.T) {
+
+}
